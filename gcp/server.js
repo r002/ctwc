9769664bec82940f69157b7415df8d7243d22f05
Robert Lin <rcl2106@gmail.com>
2020-12-19 21:14:47 -0500

    Update .gitignore and update the player profile image shower.

--- gcp/server.js
+++ gcp/server.js
@@ -1,8 +1,39 @@
+const e = require('express')
 const express = require('express')
 const app = express()
 const fetch = require('node-fetch')
 
-app.get('/player/image/:player_name', (req, res) => get_player_img(req, res))
+app
+  .get('/v1/player/:player_name', (req, res) => get_player(req, res))
+  .get('/v1/player/:player_name/image', (req, res) => get_player_img(req, res))
+
+
+get_player = async (req, res) => {
+  // res.send(`>> Player Fetched: ${req.params.player_name}`)
+
+  let url = "https://r002.github.io/ctwc/db/players.json"
+  let settings = { method: "Get" }
+  fetch(url, settings)
+    .then(res => res.json())
+    .then((player_list) => {
+      // console.log(">> RETURNED", player_list)
+
+      result = player_list.filter(player => {
+        return player.player_code === req.params.player_name
+      })
+
+      if (result.length>0) {
+        result[0].image = `https://ctwc-cloud.appspot.com/v1/player/${req.params.player_name}/image`
+      } else
+      {
+        result = "No player with that player_code found."
+      }
+
+      res.send(result)
+  })
+
+}
+
 
 get_player_img = async (req, res) => {
   const url = `https://r002.github.io/ctwc/img/player/${req.params.player_name}.png`
